4de21c3432db288aa66c096c447d31d98185d894
f-lat <l181127@lhr.nu.edu.pk>
2021-03-12 19:13:33 +0500

    scoreboard recognition works now

--- main.py
+++ main.py
@@ -3,42 +3,53 @@ import numpy as np
 import pytesseract
 import os
 
+# C:\Program Files\Tesseract-OCR
 
-#percentage accuracy
-per = 35
-
-
-#reading the query image, which is my template
-imgQ = cv2.imread("template.png")
-h,w,c = imgQ.shape
-imgQ = cv2.resize(imgQ, (w//2, h//2))
+per = 5
 
 
-#using the orb to detect keypoints
-orb = cv2.ORB_create(5000)
-kp1, des1 = orb.detectAndCompute(imgQ, None)
-
-img = cv2.imread("test1.jpg")
-kp2, des2 = orb.detectAndCompute(img, None)
-bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-
-#imgKp1 = cv2.drawKeypoints(imgQ, kp1, None)
-#cv2.imshow("kp1",imgKp1 )
+#roi = pass
 
 
-matches = bf.match(des2, des1)
-matches.sort(key = lambda x: x.distance)
-good = matches[:int(len(matches)*(per/100))]
-imgMatch = cv2.drawMatches(img, kp2, imgQ, kp1, good, None, flags=2)
-imgMatch = cv2.resize(imgMatch, (w//2, h//2))
-cv2.imshow("test", imgMatch)
 
-srcPoints = np.float32([kp2[m.queryIdx].pt for m in good]).reshape(-1,1,2)
-dstPoints = np.float32([kp1[m.trainIdx].pt for m in good]).reshape(-1,1,2)
 
-M, _ = cv2.findHomography(srcPoints, dstPoints, cv2.RANSAC, 5.0)
-imgScan = cv2.warpPerspective(img, M, (w,h))
-cv2.imshow("final", imgScan)
+#pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-#cv2.imshow("template", imgQ)
+imgQ = cv2.imread('Query.png')
+imgQ = cv2.cvtColor(imgQ, cv2.COLOR_BGR2GRAY)
+h,w = imgQ.shape
+#imgQ = cv2.resize(imgQ,(w//3,h//3))
+
+orb = cv2.ORB_create(1000)
+kp1, des1 = orb.detectAndCompute(imgQ,None)
+#impKp1 = cv2.drawKeypoints(imgQ,kp1,None)
+
+path = 'scoreboards'
+myPicList = os.listdir(path)
+print(myPicList)
+
+for j,y in enumerate(myPicList):
+    img = cv2.imread(path +"/"+y)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #img = cv2.resize(img, (w // 3, h // 3))
+    # cv2.imshow(y, img)
+    kp2, des2 = orb.detectAndCompute(img,None)
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+    matches = bf.match(des2,des1)
+    matches.sort(key= lambda x: x.distance)
+    good = matches[:int(len(matches)*(per/100))]
+    imgMatch = cv2.drawMatches(img,kp2,imgQ,kp1,good[:100],None,flags=2)
+
+    # cv2.imshow(y, imgMatch)
+
+    srcPoints = np.float32([kp2[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dstPoints = np.float32([kp1[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+    M, _ = cv2.findHomography(srcPoints,dstPoints,cv2.RANSAC,5.0)
+    imgScan = cv2.warpPerspective(img,M,(w,h))
+
+    imgScan = cv2.resize(imgScan, (w,h))
+    cv2.imshow(y, imgScan)
+
+
 cv2.waitKey(0)
