f2d3c904b5c09b31b6d5447a74e7bce905a484f0
Salman <61201330+f-lat@users.noreply.github.com>
2021-10-26 23:06:36 +0500

    commited again

--- main.py
+++ main.py
@@ -30,28 +30,29 @@ myPicList = os.listdir(path)
 print(myPicList)
 
 for j,y in enumerate(myPicList):
-    img = cv2.imread(path +"/"+y)
-    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #img = cv2.resize(img, (w // 3, h // 3))
-    # cv2.imshow(y, img)
-    kp2, des2 = orb.detectAndCompute(img,None)
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
-    matches = bf.match(des2,des1)
-    matches.sort(key= lambda x: x.distance)
-    good = matches[:int(len(matches)*(per/100))]
-    imgMatch = cv2.drawMatches(img,kp2,imgQ,kp1,good[:100],None,flags=2)
+	img = cv2.imread(path +"/"+y)
+	#img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	#img = cv2.resize(img, (w // 3, h // 3))
+	# cv2.imshow(y, img)
+	kp2, des2 = orb.detectAndCompute(img,None)
+	bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+	matches = bf.match(des2,des1)
+	matches = list(matches)
+	matches.sort(key= lambda x: x.distance)
+	good = matches[:int(len(matches)*(per/100))]
+	imgMatch = cv2.drawMatches(img,kp2,imgQ,kp1,good[:10],None,flags=2)
 
-    cv2.imshow(y, imgMatch)
+	cv2.imshow(y, imgMatch)
 
-    srcPoints = np.float32([kp2[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-    dstPoints = np.float32([kp1[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+	srcPoints = np.float32([kp2[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+	dstPoints = np.float32([kp1[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
 
-    M, _ = cv2.findHomography(srcPoints,dstPoints,cv2.RANSAC,5.0)
-    imgScan = cv2.warpPerspective(img,M,(w,h))
+	M, _ = cv2.findHomography(srcPoints,dstPoints,cv2.RANSAC,5.0)
+	imgScan = cv2.warpPerspective(img,M,(w,h))
 
-    imgScan = cv2.resize(imgScan, (w,h))
-    cv2.imshow(y, imgScan)
-    cv2.imwrite(str("Results/" + str(y)), imgScan)
+	imgScan = cv2.resize(imgScan, (w,h))
+	cv2.imshow(y, imgScan)
+	cv2.imwrite(str("Results/" + str(y)), imgScan)
 
 
 cv2.waitKey(0)
